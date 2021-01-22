14f67a901fee4b474df1820a819762ef4294a281
umar-mohammad <umar.mohammad@kcl.ac.uk>
2021-01-22 00:11:41 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import logo from "./logo.svg";
+import React, { Component } from "react";
+import "./App.css";
+let defaultTextColor = "#fff";
+let defaultStyle = {
+    color: defaultTextColor
+};
+
+class Aggregate extends Component {
+  render() {
+    return (
+      <div style={{...defaultStyle, width: "40%", display: 'inline-block'}}>
+        <h2 style={{ color: "#fff" }}>Number Text</h2>
+      </div>
+    );
+  }
+}
+
+class Filter extends Component {
+  render() {
+    return (
+      <div style={defaultStyle}>
+        <img />
+        <input type="text" />
+      </div>
+    );
+  }
+}
+
+class Playlist extends Component {
+  render() {
+    return (
+      <div style={{...defaultStyle, width: "25%", display: 'inline-block'}}>
+        <img />
+        <h3>Playlist Name</h3>
+        <ul>
+          <li>Song 1</li>
+          <li>Song 2</li>
+          <li>Song 3</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Title</h1>
+        <Aggregate />
+        <Aggregate />
+        <Filter />
+        <Playlist />
+        <Playlist />
+        <Playlist />
+        <Playlist />
+      </div>
+    );
+  }
+}
+
+export default App;
